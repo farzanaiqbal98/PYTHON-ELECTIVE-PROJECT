b991ef5d70ca016b682081b4bac1f27254db95cb
farzanaiqbal98 <65777860+farzanaiqbal98@users.noreply.github.com>
2020-05-22 22:18:34 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,24 @@
+TITLE = "UP N UP!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+FONT_NAME = 'Arial'
+HS_FILE = "highscore.txt"
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 24
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40),
+                 (WIDTH / 2 - 60, HEIGHT * 3 / 4 - 50),
+                 (125, HEIGHT - 350),
+                 (300, 150),
+                 (175, 100)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+txt1=(16,78,139)
